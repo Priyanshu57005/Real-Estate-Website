2cb76c1cbaeb064df84c34feeff24fdedb4e2aed
Priyanshu Gautam <priyanshu57005@gmail.com>
2025-09-05 21:19:46 +0530

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,9 +32,7 @@ const About = () => {
           If you're looking to invest in real estate or need expert guidance, we invite you to join our ever-growing community of satisfied clients.</p>
         </div>
       </div>
-      <footer>
-    <p>&copy; 2025 Real-Esate Promo Website | Estate sourced from public domain or used under license.</p>
-    </footer>
+      
     </div>
     
     
